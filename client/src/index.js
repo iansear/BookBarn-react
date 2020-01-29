c1ecfce8d3d66eefdd0fd72bebd1cec28db6b0cc
mehmet ozek <mehmetozek@mehmets-MacBook-Air.local>
2020-01-29 11:53:23 -0600

    nargiza branch

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import BaseLayout from './components/BaseLayout';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+   <BaseLayout>
+    <Switch>
+        <Route exact path = "/"  component = {App} />
+        <Route path = "/about"  component = {AboutUs} />
+        <Route path = "/posts/:postId" component = {PostDetails} />
+    </Switch>
+   </BaseLayout>
+</BrowserRouter>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
