066ff1f3cb6a6d473f592444b391e7d6db17b7da
mehmet ozek <mehmetozek@mehmets-MacBook-Air.local>
2020-01-30 09:12:29 -0600

    deleted and added data

--- server/app.js
+++ server/app.js
@@ -1,12 +1,7 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = 3001
 global.models = require('./models')
-const testAPIRouter = require('./routes/testAPI')
-
-
-
-app.use('/testAPI', testAPIRouter)
 
 
 
@@ -14,18 +9,18 @@ app.listen(PORT, () => {
   console.log("Server is running...")
 })
 
-app.post("/", (req, res) => {
-  const book = models.Book.build({
-    author: "DataTypes.STRING",
-    country: "DataTypes.STRING",
-    imageLink: "DataTypes.STRING",
-    language: "DataTypes.STRING",
-    link: "DataTypes.STRING",
-    pages: 0,
-    title: "DataTypes.STRING",
-    year: 0
-  })
-  book.save().then( async () => {
-      res.redirect("/")
-  })
-})
+// app.post("/", (req, res) => {
+//   const book = models.Book.build({
+//     author: "DataTypes.STRING",
+//     country: "DataTypes.STRING",
+//     imageLink: "DataTypes.STRING",
+//     language: "DataTypes.STRING",
+//     link: "DataTypes.STRING",
+//     pages: 0,
+//     title: "DataTypes.STRING",
+//     year: 0
+//   })
+//   book.save().then( async () => {
+//       res.redirect("/")
+//   })
+// })
