f4e18f8ff905562f76b56a1e54ba639e6f9ef96e
mehmet ozek <mehmetozek@mehmets-MacBook-Air.local>
2020-02-04 17:00:23 -0600

    login, toggle, and fav global state is done

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import BaseLayout from './components/BaseLayout';
 import BookDetails from './components/BookDetails';
 import Register from './components/Register'
+import Login from './components/Login'
 import UpdateBook from './components/UpdateBook';
 import {createStore} from 'redux'
 import { Provider } from 'react-redux'
@@ -19,10 +20,12 @@ ReactDOM.render(
         <BrowserRouter>
         <BaseLayout>
         <Switch>
+            
             <Route exact path = "/"  component = {Books} />
             <Route path="/bookdetails/:bookID" component = {BookDetails} />
             <Route path="/updatebook/:bookID" component = {UpdateBook} />
             <Route path="/join" component={Register} />
+            <Route path="/sign-in" component={Login}/>
         </Switch>
         </BaseLayout>
     </BrowserRouter>
